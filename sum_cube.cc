c5730ff3c4ca4d8323a90f71847e2700b4939441
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-12 17:12:55 +0800

    n

--- sum_cube.cc
+++ sum_cube.cc
@@ -1,5 +1,6 @@
 #include"stdio.h"
 #include"stdlib.h"
+#include"math.h"
 #include<iostream>
 
 using namespace std;
@@ -17,8 +18,7 @@ int* f(int len)
     cal=i;
     while (cal)
     {
-      temp=cal%10;
-      sum+=temp*temp*temp;
+      sum+=pow((cal%10),3);
       cal/=10;
     }
     if (sum == i)
